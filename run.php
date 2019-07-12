6898d7c06f045db2e8242ee9c3da92b8a83d8886
Justin Bull <bull.justin@gmail.com>
2019-07-11 18:11:42 -0700

    Refactor code into class methods with documentation

--- run.php
+++ run.php
@@ -1,16 +1,11 @@
-<?php
+<?php declare(strict_types=1);
 
-declare(strict_types = 1);
+use GeoLocationExtractor\ImageWorker;
 
-try {
-    $isLoaded = extension_loaded('exif');
-    if (!$isLoaded) {
-        throw new Exception('Exif extension is required.');
-    }
-} catch (Throwable $exception) {
-    $message = $exception->getMessage() . PHP_EOL;
-    die($message);
-}
+spl_autoload_register(function (string $class): void {
+    $filename = str_replace('\\', '/', $class);
+    include "src/{$filename}.php";
+});
 
 $shortOpts = 'd:';
 $longOpts = ['directory:'];
@@ -19,109 +14,11 @@ $directory = $options['d'] ?? $options['directory'] ?? 'gps_images';
 $directory = realpath($directory);
 
 try {
-    $directoryExists = file_exists($directory);
-    if (!$directoryExists) {
-        throw new Exception('Directory does not exist.');
-    }
+    $imageWorker = new ImageWorker($directory);
+    $imageWorker->getValidPaths();
+    $imageWorker->getGeoLocationData();
+    $imageWorker->outputToFile();
 } catch (Throwable $exception) {
     $message = $exception->getMessage() . PHP_EOL;
     die($message);
 }
-
-$acceptedFiles = [];
-$acceptedFileExtensions = [
-    'jpeg',
-    'jpg',
-];
-
-try {
-    $directoryIterator = new RecursiveDirectoryIterator($directory);
-    $fileIterator = new RecursiveIteratorIterator($directoryIterator);
-    foreach($fileIterator as $file) {
-        if ($file instanceof SplFileInfo) {
-            $fileExtension = $file->getExtension();
-            $isAcceptedExtension = in_array($fileExtension, $acceptedFileExtensions);
-            if ($file->isFile() && $isAcceptedExtension) {
-                $acceptedFiles[] = $file->getRealPath();
-            }
-        } else {
-            throw new Exception('Unexpected object type.');
-        }
-    }
-} catch (Throwable $exception) {
-    $message = $exception->getMessage() . PHP_EOL;
-    die($message);
-}
-
-try {
-    $filesExist = count($acceptedFiles) > 0;
-    if (!$filesExist) {
-        throw new Exception('No files found.');
-    }
-} catch (Throwable $exception) {
-    $message = $exception->getMessage() . PHP_EOL;
-    die($message);
-}
-
-$gpsData = [];
-
-foreach($acceptedFiles as $filePath) {
-    $exifData = exif_read_data($filePath);
-    $longitude = $exifData['GPSLongitude'] ?? [];
-    $longitudeRef = $exifData['GPSLongitudeRef'] ?? '';
-    $latitude = $exifData['GPSLatitude'] ?? [];
-    $latitudeRef = $exifData['GPSLatitudeRef'] ?? '';
-    if ($longitude && $longitudeRef && $latitude && $latitudeRef) {
-        $gpsData[] = [
-            'filename'           => str_replace("{$directory}/", '', $filePath),
-            'longitude'      => getDecimalCoordinate($longitude, $longitudeRef),
-            'latitude'       => getDecimalCoordinate($latitude, $latitudeRef),
-            'humanLongitude' => getFormattedCoordinate($longitude, $longitudeRef),
-            'humanLatitude'  => getFormattedCoordinate($latitude, $latitudeRef),
-        ];
-    }
-}
-
-function getDecimalCoordinate(array $coordinates, string $hemisphere): float {
-    $degrees = count($coordinates) > 0 ? convertCoordinate($coordinates[0]) : 0;
-    $minutes = count($coordinates) > 1 ? convertCoordinate($coordinates[1]) : 0;
-    $seconds = count($coordinates) > 2 ? convertCoordinate($coordinates[2]) : 0;
-    $flip = ($hemisphere == 'W' or $hemisphere == 'S') ? -1 : 1;
-    return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
-}
-
-function getFormattedCoordinate(array $coordinates, string $hemisphere): string {
-    $degrees = count($coordinates) > 0 ? convertCoordinate($coordinates[0]) : 0;
-    $minutes = count($coordinates) > 1 ? convertCoordinate($coordinates[1]) : 0;
-    $seconds = count($coordinates) > 2 ? convertCoordinate($coordinates[2]) : 0;
-    return "{$degrees}\u{00B0} {$minutes}\u{0027} {$seconds}\u{0022} {$hemisphere}";
-}
-
-function convertCoordinate(string $coordinate): float {
-    $parts = explode('/', $coordinate);
-    $decimalNumber = floatval($parts[0]);
-    $decimalPlace = floatval($parts[1]);
-    return $decimalNumber / $decimalPlace;
-}
-
-try {
-    $dataExist = count($gpsData) > 0;
-    if (!$dataExist) {
-        throw new Exception('No gps data was found.');
-    }
-} catch (Throwable $exception) {
-    $message = $exception->getMessage() . PHP_EOL;
-    die($message);
-}
-
-$filePointer = fopen('output.csv', 'w');
-
-$headers = array_keys(reset($gpsData));
-fputcsv($filePointer, $headers);
-
-foreach ($gpsData as $fields) {
-    $fields = array_values($fields);
-    fputcsv($filePointer, $fields);
-}
-
-fclose($filePointer);
---
+++ src/GeoLocationExtractor/ImageWorker.php
@@ -0,0 +1,176 @@
+<?php declare(strict_types=1);
+
+namespace GeoLocationExtractor;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
+use Exception;
+
+class ImageWorker
+{
+    private const VALID_FILE_EXTENSIONS = [
+        'jpeg',
+        'jpg',
+    ];
+
+    private $directory;
+    private $validFiles = [];
+    private $gpsData = [];
+
+    /**
+     * ImageWorker constructor.
+     *
+     * @param string $directory
+     *
+     * @throws \Exception
+     */
+    public function __construct(string $directory)
+    {
+        $this->directory = $directory;
+
+        $directoryExists = file_exists($this->directory);
+        if (!$directoryExists) {
+            throw new Exception('Directory does not exist.');
+        }
+
+        $isLoaded = extension_loaded('exif');
+        if (!$isLoaded) {
+            throw new Exception('Exif extension is required.');
+        }
+    }
+
+    /**
+     * Retrieves a list of all valid images found within the provided directory.
+     *
+     * @throws \Exception
+     */
+    public function getValidPaths(): void
+    {
+        $directoryIterator = new RecursiveDirectoryIterator($this->directory);
+        $fileIterator = new RecursiveIteratorIterator($directoryIterator);
+
+        foreach ($fileIterator as $file) {
+            if ($file instanceof SplFileInfo) {
+                $fileExtension = $file->getExtension();
+                $isValidExtension = in_array($fileExtension, ImageWorker::VALID_FILE_EXTENSIONS);
+                if ($file->isFile() && $isValidExtension) {
+                    $this->validFiles[] = $file->getRealPath();
+                }
+            } else {
+                throw new Exception('Unexpected object type.');
+            }
+        }
+    }
+
+    /**
+     * Recurse over images and retrieve their geolocation data from exif.
+     *
+     * @throws \Exception
+     */
+    public function getGeoLocationData(): void
+    {
+        $filesExist = count($this->validFiles) > 0;
+        if (!$filesExist) {
+            throw new Exception('No files found.');
+        }
+
+        foreach ($this->validFiles as $filePath) {
+            $exifData = exif_read_data($filePath);
+            $longitude = $exifData['GPSLongitude'] ?? [];
+            $longitudeRef = $exifData['GPSLongitudeRef'] ?? '';
+            $latitude = $exifData['GPSLatitude'] ?? [];
+            $latitudeRef = $exifData['GPSLatitudeRef'] ?? '';
+            if ($longitude && $longitudeRef && $latitude && $latitudeRef) {
+                $this->gpsData[] = [
+                    'filename'       => str_replace("{$this->directory}/", '', $filePath),
+                    'longitude'      => $this->getDecimalCoordinate($longitude, $longitudeRef),
+                    'latitude'       => $this->getDecimalCoordinate($latitude, $latitudeRef),
+                    'humanLongitude' => $this->getFormattedCoordinate($longitude, $longitudeRef),
+                    'humanLatitude'  => $this->getFormattedCoordinate($latitude, $latitudeRef),
+                ];
+            }
+        }
+    }
+
+    /**
+     * Writes all geolocation data to a csv file.
+     *
+     * @throws \Exception
+     */
+    public function outputToFile(): void
+    {
+        $dataExist = count($this->gpsData) > 0;
+        if (!$dataExist) {
+            throw new Exception('No gps data was found.');
+        }
+
+        $outputFile = getcwd() . '/output.csv';
+        $filePointer = fopen($outputFile, 'w');
+
+        $headers = array_keys(reset($this->gpsData));
+        fputcsv($filePointer, $headers);
+
+        foreach ($this->gpsData as $fields) {
+            $fields = array_values($fields);
+            fputcsv($filePointer, $fields);
+        }
+
+        fclose($filePointer);
+    }
+
+    /**
+     * Converts degrees, minutes, seconds (DMS) coordinates into decimal degrees (dd).
+     *
+     * @param array  $coordinates
+     * @param string $hemisphere
+     *
+     * @return float
+     */
+    private function getDecimalCoordinate(array $coordinates, string $hemisphere): float
+    {
+        $degrees = count($coordinates) > 0 ? $this->convertCoordinate($coordinates[0]) : 0;
+        $minutes = count($coordinates) > 1 ? $this->convertCoordinate($coordinates[1]) : 0;
+        $seconds = count($coordinates) > 2 ? $this->convertCoordinate($coordinates[2]) : 0;
+
+        $flip = ($hemisphere == 'W' or $hemisphere == 'S') ? -1 : 1;
+
+        return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+    }
+
+    /**
+     * Converts degrees, minutes, seconds (DMS) coordinates into a standard format for humans.
+     * Format: d° m' s" {N|S}, d° m' s" {E|W}
+     *
+     * @param array  $coordinates
+     * @param string $hemisphere
+     *
+     * @return string
+     */
+    private function getFormattedCoordinate(array $coordinates, string $hemisphere): string
+    {
+        $degrees = count($coordinates) > 0 ? $this->convertCoordinate($coordinates[0]) : 0;
+        $minutes = count($coordinates) > 1 ? $this->convertCoordinate($coordinates[1]) : 0;
+        $seconds = count($coordinates) > 2 ? $this->convertCoordinate($coordinates[2]) : 0;
+
+        return "{$degrees}\u{00B0} {$minutes}\u{0027} {$seconds}\u{0022} {$hemisphere}";
+    }
+
+    /**
+     * Decimals are represented by two values, the number and the decimal place.
+     * This function will parse these values and provide the decimal number.
+     * Ex: 12345/100 -> 123.45
+     *
+     * @param string $coordinate
+     *
+     * @return float
+     */
+    private function convertCoordinate(string $coordinate): float
+    {
+        $parts = explode('/', $coordinate);
+        $decimalNumber = floatval($parts[0]);
+        $decimalPlace = floatval($parts[1]);
+
+        return $decimalNumber / $decimalPlace;
+    }
+}
